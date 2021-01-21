6169336fd11f9629dfed0f4b377ae1ec445b2b8b
yumazd <yumazd@gmail.com>
2021-01-21 11:43:52 +0900

    確保病床利用率と宿泊療養施設利用率を追加

--- components/MonitoringItemsOverviewTableMedicalSystem.vue
+++ components/MonitoringItemsOverviewTableMedicalSystem.vue
@@ -18,6 +18,20 @@
             />
           </div>
         </li>
+        <li :class="[$style.box]">
+          <div :class="$style.content">
+            <span>確保病床利用率</span>
+            <monitoring-items-overview-table-value-with-translatable-unit
+              :value="
+                retInpatientRatio(
+                  items.入院患者数.value,
+                  items.入院患者確保病床数.value
+                ).value
+              "
+              :unit="unit"
+            />
+          </div>
+        </li>
       </ul>
     </li>
     <li :class="[$style.box, $style.parent]">
@@ -38,6 +52,20 @@
             />
           </div>
         </li>
+        <li :class="[$style.box]">
+          <div :class="$style.content">
+            <span>宿泊療養施設利用率</span>
+            <monitoring-items-overview-table-value-with-translatable-unit
+              :value="
+                retInpatientRatio(
+                  items.宿泊療養施設療養者数.value,
+                  items.宿泊療養施設確保室数.value
+                ).value
+              "
+              :unit="unit"
+            />
+          </div>
+        </li>
       </ul>
     </li>
   </ul>
@@ -48,6 +76,7 @@ import Vue, { PropType } from 'vue'
 
 import MonitoringItemsOverviewTableValueWithTranslatableUnit from '@/components/MonitoringItemsOverviewTableValueWithTranslatableUnit.vue'
 import { MonitoringItems } from '@/utils/formatMonitoringItems'
+import { getCommaSeparatedNumberToFixedFunction } from '@/utils/monitoringStatusValueFormatters'
 
 export default Vue.extend({
   components: {
@@ -57,6 +86,26 @@ export default Vue.extend({
     items: {
       type: Object as PropType<MonitoringItems>,
       required: true,
+    },
+  },
+  data() {
+    return {
+      unit: { text: '%', translatable: false },
+    }
+  },
+  methods: {
+    retInpatientRatio(item1: string, item2: string) {
+      // const data = this.items
+      const patients: number = parseInt(item1, 10)
+      const bedNum: number = parseInt(item2, 10)
+      const toNumberInHundredthsPlace = getCommaSeparatedNumberToFixedFunction(
+        1
+      )
+      const calc = (patients / bedNum) * 100
+      const retVal: object = {
+        value: toNumberInHundredthsPlace(calc),
+      }
+      return retVal
     },
   },
 })
